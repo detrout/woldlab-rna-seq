0781f1d0d285defe5a570792c4b8b356c01b5a4a
Diane Trout <diane@ghic.org>
2016-07-29 16:16:19 -0700

    Support saving quantification files in tsv and csv

--- woldrnaseq/makersemcsv.py
+++ woldrnaseq/makersemcsv.py
@@ -17,15 +17,20 @@ def main(cmdline=None):
     experiments = models.load_experiments([args.experiments], sep=sep)
     libraries = models.load_library_tables([args.libraries], sep=sep)
 
+    output_sep = models.get_seperator(args.output_format)
+    output_extension = {
+        'TAB': '.tsv',
+        ',': '.csv',
+    }[args.output_format]
+
     for name in experiments:
-        filename = name + '_' + args.quantification + '.csv'
+        filename = name + '_' + args.quantification + output_extension
         replicates = experiments[name]
         logger.info("%s %s: %s",
                     name, args.quantification, ','.join(replicates))
         quantifications = madqc.load_replicates(
             replicates, libraries, args.quantification)
-        quantifications.to_csv(filename)
-
+        quantifications.to_csv(filename, sep=output_sep)
 
 def make_parser():
     parser = argparse.ArgumentParser(
@@ -38,7 +43,7 @@ def make_parser():
     parser.add_argument('-l', '--libraries', help='library information table')
     parser.add_argument('-e', '--experiments', help='experiment information table')
     parser.add_argument('-s', '--sep', choices=['TAB', ','], default='TAB')
-
+    parser.add_argument('--output-format', choices=['TAB', ','], default=',')
     return parser
 
 if __name__ == '__main__':
