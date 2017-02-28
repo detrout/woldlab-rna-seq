8a632308d6cf7f07f95f2931e574dc44850cbf3b
Diane Trout <diane@ghic.org>
2017-02-27 16:08:22 -0800

    Use a better shell test for if a bam file is paired.

--- woldrnaseq/sort-samtools.sh
+++ woldrnaseq/sort-samtools.sh
@@ -6,7 +6,7 @@ export MEMORY=$3
 export THREADS=$4
 export PAIRED=$5
 
-if [ -z ${PAIRED} ]; then
+if [ ${PAIRED} -eq 0 ]; then
     echo "sort single end to ${OUT}"
   cat <( samtools view -H ${IN} ) \
       <( samtools view ${IN}  | \
