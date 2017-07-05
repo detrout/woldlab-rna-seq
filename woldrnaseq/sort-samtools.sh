7137c0965b9f1458cad87cbc141090ce290a3a19
Diane Trout <diane@ghic.org>
2017-07-05 13:18:39 -0700

    Write temp sort files to condor scratch space

--- woldrnaseq/sort-samtools.sh
+++ woldrnaseq/sort-samtools.sh
@@ -7,16 +7,16 @@ export THREADS=$4
 export PAIRED=$5
 
 if [ -z ${PAIRED} ]; then
-    echo "sort single end to ${OUT}"
+    echo "sort single end to ${OUT} (Temp ${_CONDOR_SCRATCH_DIR})"
   cat <( samtools view -H ${IN} ) \
       <( samtools view ${IN}  | \
-         sort -S ${MEMORY} -T ./ ) | \
+         sort -S ${MEMORY} -T ${_CONDOR_SCRATCH_DIR} ) | \
       samtools view -@ ${THREADS} -bS - -o ${OUT}
 else
-    echo "sort paired end to ${OUT}"
+    echo "sort paired end to ${OUT} (Temp ${_CONDOR_SCRATCH_DIR})"
   cat <( samtools view -H ${IN} ) \
       <( samtools view ${IN}  | \
          paste -d "|" - - | \
-         sort -S ${MEMORY} -T ./ | tr '|' '\n' ) | \
+         sort -S ${MEMORY} -T ${_CONDOR_SCRATCH_DIR} | tr '|' '\n' ) | \
       samtools view -@ ${THREADS} -bS - -o ${OUT}
 fi
