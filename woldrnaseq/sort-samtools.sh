5c4d917850f3655567c308fa107939e4a4378b24
Diane Trout <diane@ghic.org>
2019-02-07 16:33:36 -0800

    Include input filename in addition to output filename to logging message

--- woldrnaseq/sort-samtools.sh
+++ woldrnaseq/sort-samtools.sh
@@ -8,13 +8,13 @@ export THREADS=$4
 export PAIRED=$5
 
 if [ -z ${PAIRED} ]; then
-    echo "sort single end to ${OUT} (Temp ${_CONDOR_SCRATCH_DIR})"
+    echo "sort single end ${IN} to ${OUT} (Temp ${_CONDOR_SCRATCH_DIR})"
   cat <( samtools view -H ${IN} ) \
       <( samtools view ${IN}  | \
          sort -S ${MEMORY} -T ${_CONDOR_SCRATCH_DIR} ) | \
       samtools view -@ ${THREADS} -bS - -o ${OUT}
 else
-    echo "sort paired end to ${OUT} (Temp ${_CONDOR_SCRATCH_DIR})"
+    echo "sort paired end ${IN} to ${OUT} (Temp ${_CONDOR_SCRATCH_DIR})"
   cat <( samtools view -H ${IN} ) \
       <( samtools view ${IN}  | \
          paste -d "|" - - | \
