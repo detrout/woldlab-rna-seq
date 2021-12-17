6d69a5f6dd0b02d55f37ca7269c4d06e76aaec47
Diane Trout <diane@ghic.org>
2021-12-17 15:10:15 -0800

    Fix test name typo

--- woldrnaseq/tests/test_downloader.py
+++ woldrnaseq/tests/test_downloader.py
@@ -4,9 +4,8 @@ from woldrnaseq.downloader import (
     make_short_fastq_name
 )
 
-class TestDownlaoder(TestCase):
 
-
+class TestDownloader(TestCase):
     def test_make_short_fastq_name(self):
         self.assertEqual(
             make_short_fastq_name('22160_GAACGAAG_L002_R1_005.fastq.gz'),
@@ -16,4 +15,3 @@ class TestDownlaoder(TestCase):
             ValueError,
             make_short_fastq_name,
             '22160_GAACGAAG_R1.fastq.gz')
-    
